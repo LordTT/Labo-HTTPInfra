faf284a98f9fd22f2d5d92e389f8927596d8fc06
teo <ferraricorneloup.teo@gmail.com>
2021-05-25 17:05:03 +0200

    Part b : craetion of docker image wrapping an express.js that returns weird alien messages

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,4 +1,28 @@
 var Chance = require('chance');
 var chance = new Chance();
 
-console.log("Bonjour " + chance.name());
+var express = require('express')
+var app = express()
+var port = 3000
+
+app.get('/', (req, res) => {
+  res.send(alienMessage())
+})
+
+app.listen(port, () => {
+  console.log(`Listens on ${port}`)
+})
+
+function alienMessage(){
+	var message = [];
+	var alienName =  chance.word();
+	var alienSpeech = chance.paragraph();
+	
+	message.push({
+	name: alienName,
+	message : alienSpeech	
+	})
+	
+	console.log(message);
+	return message;
+}
