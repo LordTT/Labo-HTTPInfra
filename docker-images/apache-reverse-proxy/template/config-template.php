0005d735808c7dcccbc7c623996a4a9de03446c7
teo <ferraricorneloup.teo@gmail.com>
2021-05-28 21:18:48 +0200

    sticky sessions for the static part implemented

--- docker-images/apache-reverse-proxy/template/config-template.php
+++ docker-images/apache-reverse-proxy/template/config-template.php
@@ -4,6 +4,7 @@
     #ErrorLog ${APACHE_LOG_DIR}/error.log
     #CustomLog ${APACHE_LOG_DIR}/access.log combined
 
+	Header add Set-Cookie "ROUTEID=.%{BALANCER_WORKER_ROUTE}e; path=/" env=BALANCER_ROUTE_CHANGED
 	
     <Proxy balancer://dynamic_app>
         BalancerMember 'http://<?=getenv('DYNAMIC_APP_1')?>'
@@ -11,8 +12,10 @@
     </Proxy>
 
     <Proxy balancer://static_app>
-        BalancerMember 'http://<?=getenv('STATIC_APP_1')?>'
-        BalancerMember 'http://<?=getenv('STATIC_APP_2')?>'
+        BalancerMember 'http://<?=getenv('STATIC_APP_1')?>' route=1
+        BalancerMember 'http://<?=getenv('STATIC_APP_2')?>' route=2
+		ProxySet lbmethod=byrequests
+        ProxySet stickysession=ROUTEID
     </Proxy>
 	
     ProxyPass "/api/students" "balancer://dynamic_app/"
