9c9b1e05e67a4b3905605d93e38c1fb83acbf294
teo <ferraricorneloup.teo@gmail.com>
2021-05-28 11:59:06 +0200

    multiple server nodes implemented

--- docker-images/apache-reverse-proxy/template/config-template.php
+++ docker-images/apache-reverse-proxy/template/config-template.php
@@ -4,9 +4,20 @@
     #ErrorLog ${APACHE_LOG_DIR}/error.log
     #CustomLog ${APACHE_LOG_DIR}/access.log combined
 
-    ProxyPass "/api/students" "http://<?=getenv('DYNAMIC_APP')?>/"
-    ProxyPassReverse "/api/students" "http://<?=getenv('DYNAMIC_APP')?>/"
+	
+    <Proxy balancer://dynamic_app>
+        BalancerMember 'http://<?=getenv('DYNAMIC_APP_1')?>'
+        BalancerMember 'http://<?=getenv('DYNAMIC_APP_2')?>'
+    </Proxy>
 
-    ProxyPass "/" "http://<?=getenv('STATIC_APP')?>/"
-    ProxyPassReverse "/" "http://<?=getenv('STATIC_APP')?>/"
+    <Proxy balancer://static_app>
+        BalancerMember 'http://<?=getenv('STATIC_APP_1')?>'
+        BalancerMember 'http://<?=getenv('STATIC_APP_2')?>'
+    </Proxy>
+	
+    ProxyPass "/api/students" "balancer://dynamic_app/"
+    ProxyPassReverse "/api/students" "balancer://dynamic_app/"
+
+    ProxyPass "/" "balancer://static_app/"
+    ProxyPassReverse "/" "balancer://static_app/"
 </VirtualHost>
